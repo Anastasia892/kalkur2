8f1b2ddfbdc1ab71fe11831c99e6e85a4d85e742
Владелец <@DESKTOP-1NJN28O>
2022-02-06 21:58:17 +0300

    Добавьте файлы проекта.

---
+++ ViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace kalkur2
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string ItogChanged
+        {
+            get
+            {
+                return model.itog.ToString();
+            }
+        }
+
+        public List<string> cbox
+        {
+            get
+            {
+                return model.datalist;
+            }
+        }
+
+        public static float oneSV
+        {
+            set
+            {
+                model.one = value;
+            }
+        }
+
+        public static float twoSV
+        {
+            set
+            {
+                model.two = value;
+            }
+        }
+
+        public static int idSV
+        {
+            set
+            {
+                model.id = value;
+            }
+        }
+
+        public RoutedCommand Command { get; set; } = new RoutedCommand();
+        public void Command_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            switch (model.id)
+            {
+                case 0:
+                    model.itog = model.one + model.two;
+                    break;
+                case 1:
+                    model.itog = model.one * model.two;
+                    break;
+                case 2:
+                    model.itog = model.one - model.two;
+                    break;
+                case 3:
+                    model.itog = model.one / model.two;
+                    break;
+            }
+
+
+
+            PropertyChanged(this, new PropertyChangedEventArgs("ItogChanged"));
+        }
+
+        public CommandBinding bind;
+        public ViewModel()
+        {
+            bind = new CommandBinding(Command);
+            bind.Executed += Command_Executed;
+        }
+    }
+}
---
+++ model.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kalkur2
+{
+   
+        static class model
+        {
+            public static float one = 0;
+            public static float two = 0;
+            public static int id = 0;
+            public static float itog;
+            public static List<string> datalist = new List<string> { "Сложение", "Умножение", "Вычитание", "Деление" };
+
+
+
+        }
+    
+}
